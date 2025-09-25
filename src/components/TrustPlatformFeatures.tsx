29214192ace433bb957b26501cc3338897472a68
JFK465 <info@jfkconsulting.de>
2025-09-25 13:06:37 +0200

    Farbe

--- src/components/TrustPlatformFeatures.tsx
+++ src/components/TrustPlatformFeatures.tsx
@@ -89,14 +89,14 @@ const Feature = ({
         index >= 3 && "lg:border-b-0"
       )}
     >
-      <div className="opacity-0 group-hover/feature:opacity-100 transition duration-300 absolute inset-0 h-full w-full bg-gradient-to-b from-blue-50 to-transparent pointer-events-none" />
+      <div className="opacity-0 group-hover/feature:opacity-100 transition duration-300 absolute inset-0 h-full w-full bg-gradient-to-b from-[#e24e1b]/5 to-transparent pointer-events-none" />
 
-      <div className="mb-4 relative z-10 text-blue-600 group-hover/feature:text-blue-700 transition-colors duration-300">
+      <div className="mb-4 relative z-10 text-[#e24e1b] group-hover/feature:text-[#d63f14] transition-colors duration-300">
         {icon}
       </div>
 
       <div className="text-lg font-bold mb-2 relative z-10">
-        <div className="absolute left-0 inset-y-0 h-6 group-hover/feature:h-8 w-1 rounded-tr-full rounded-br-full bg-gray-300 group-hover/feature:bg-blue-600 transition-all duration-300 origin-center" />
+        <div className="absolute left-0 inset-y-0 h-6 group-hover/feature:h-8 w-1 rounded-tr-full rounded-br-full bg-gray-300 group-hover/feature:bg-[#e24e1b] transition-all duration-300 origin-center" />
         <span className="group-hover/feature:translate-x-2 transition duration-300 inline-block text-gray-900 pl-4">
           {title}
         </span>
